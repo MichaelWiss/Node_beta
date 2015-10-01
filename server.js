9a892e761b02d52606e05a3c373e8e385047e3e5
Michael Wiss <michael.wiss@gmail.com>
2015-09-30 19:13:17 -0500

    added router

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 var http = require("http");
+var url = require("url");
 
-http.createServer(function(request, response) {
+
+function start(route) {
+  function onRequest(request, response) {
+  	var pathname = url.parse(request.url).pathname;
+	console.log("Request for "+ pathname +" received.");
+
+	route(pathname);
+	
 	response.writeHead(200, {"Content_Type": "text/plain"});
 	response.write("Hello World");
 	response.end();
-}).listen(8888);
+}
+
+http.createServer(onRequest).listen(8888);
+
+console.log("Server has started.");
+
+}
+
+exports.start = start
